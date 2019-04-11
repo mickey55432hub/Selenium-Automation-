49eb60d28901ecc1760331a7f21a25f912f36e0d
Shivam kumar tiwari <shivam55432@rediffmail.com>
2019-04-11 16:03:14 +0530

    Update SampleCode(ElementById).java

--- SampleCode(ElementById).java
+++ SampleCode(ElementById).java
@@ -1,4 +1,5 @@
 package - - - - - - -- - - - ;
+//https://www.guru99.com/first-webdriver-script.html
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,9 +12,27 @@ public class SampleCode1 {
         String tagName = "";
         
         driver.get(baseUrl);
-        tagName = driver.findElement(By.id("email")).getTagName();
+        //driver.switchTo().frame("- - -  -"); 
+         //driver.switchTo().alert();
+        //switiching between various frmaes in browser.
+        //switiching between various alerts in browser.
+         tagName = driver.findElement(By.id("email")).getTagName();
+        //By.className
+        //By.cssSelector
+        //By.linkText
+        //By.name
+        //By.partialLinkText
+        //By.tagName
+        //By.xpath
+        //driver.findElement(By.id("login")).click()
+        //to extract the tag name of that particular element whose id is "email". 
         System.out.println(tagName);
+        
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         driver.close();
+        //Delete the current window.
+        driver.quit();
+        //will close all the windows.
         System.exit(0);
 }
 }
